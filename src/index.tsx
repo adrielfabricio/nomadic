0ea6be6d17ee183280b433dbfed056a9bf516ec4
Adriel Fabrício <adrielfabricios@gmail.com>
2021-03-07 11:31:40 -0300

    feat(init): first commit details:  - configure project lint;  - add src/ folder.

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const App: React.FC = () => (
+	<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+		<Text>Nomadic</Text>
+	</View>
+);
+
+export default App;
